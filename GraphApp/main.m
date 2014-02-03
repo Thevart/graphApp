4b32d911d5ddf9a46ca35d8715ed19bb07ab5acf
Kévin Gomez <geek63@gmail.com>
2014-02-03 22:26:49 +0100

    Initial Commit

---
+++ GraphApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GraphApp
+//
+//  Created by Kévin Gomez on 03/02/14.
+//  Copyright (c) 2014 Kévin Gomez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
