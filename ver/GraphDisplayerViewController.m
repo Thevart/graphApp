72fded31bf13738a518110e1c65e897d4af1918e
Kévin Gomez <geek63@gmail.com>
2014-02-27 20:10:20 +0100

    Work on the sample graph selection menu

--- ver/GraphDisplayerViewController.m
+++ ver/GraphDisplayerViewController.m
@@ -8,33 +8,62 @@
 
 #import "GraphDisplayerViewController.h"
 
-@interface GraphDisplayerViewController ()
-
-@end
-
 @implementation GraphDisplayerViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+NSArray *tableData;
+NSString *file;
 
-- (void)viewDidLoad
+- (void) viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+
+    // by default, load nothing
+    file = nil;
+
+    // as no file is selected (yet), disable the open button
+    self.navigationItem.rightBarButtonItem.enabled = false;
+
+    // load the graph samples files list
+    NSError* error;
+    NSString* graphSamplesPath = [[NSBundle mainBundle] resourcePath];
+    graphSamplesPath = [graphSamplesPath stringByAppendingPathComponent:@"samples"];
+    tableData = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:graphSamplesPath error:&error];
 }
 
-- (void)didReceiveMemoryWarning
+- (void) didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)openAlgo:(id)sender {
+- (IBAction) openAlgo:(id) sender
+{
+    NSLog(@"Load %@", file);
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (void) tableView:(UITableView *) tableView didSelectRowAtIndexPath:(NSIndexPath *) indexPath
+{
+    file = [tableData objectAtIndex:indexPath.row];
+
+    self.navigationItem.rightBarButtonItem.enabled = true;
+}
+
 @end
